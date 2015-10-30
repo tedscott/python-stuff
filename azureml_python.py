96ee86e91243ed8481c10305d2f7d0ad6bf840bb
tedscott <fishdaddyo@gmail.com>
2015-10-29 20:26:44 -0700

    updated

--- azureml_python.py
+++ azureml_python.py
@@ -28,3 +28,33 @@ def azureml_main(frame1):
     frame1["Total.prod"] = frame1["Cottagecheese.Prod"]+frame1["Icecream.Prod"]+frame1["Milk.Prod"]
     
     return frame1
+    
+    # The script MUST contain a function named azureml_main
+# which is the entry point for this module.
+#
+# The entry point function can contain up to two input arguments:
+#   Param<dataframe1>: a pandas.DataFrame
+#   Param<dataframe2>: a pandas.DataFrame
+def azureml_main(frame1):
+    import matplotlib
+    matplotlib.use('agg')
+    import pandas as pd
+    import matplotlib.pyplot as plt
+    from pandas.tools.plotting import scatter_matrix
+    
+    ## remove unwanted columns
+    frame1.drop(["X","Y","month","day"], axis=1, inplace=True)
+    
+    ## create a scatter plot matrix
+    fig1=plt.figure(1,figsize=(12,9))
+    ax=fig1.gca()
+    scatter_matrix(frame1, alpha=0.2, figsize=(10,10),diagonal='kde',ax=ax)
+    fig1.savefig('scatter.png')
+
+
+    
+    # Return value must be of a sequence of pandas.DataFrame
+    return frame1
+
+
+    
